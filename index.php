e17f0c55b8278806c61e65671beaa3eb30ee2e37
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-04-28 16:57:10 +0200

    typos; comments

--- index.php
+++ index.php
@@ -7,19 +7,19 @@ ini_set('display_errors', 'On');
 <html lang="de">
 <head>
     <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Amazinzon</title>
+    <title>Amazingzon</title>
 
+    <!-- Load jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap CSS -->
+    <!-- Load Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous">
 </head>
-<body>
 
+<body>
 <header class="p-3 bg-dark text-white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
@@ -67,4 +67,5 @@ ini_set('display_errors', 'On');
     </div>
 </section>
 </body>
+
 </html>
