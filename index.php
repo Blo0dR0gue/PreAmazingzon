2e8bb388e9a16493074ca302b0a79c4018395649
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:04:55 +0200

    Index and Card PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Amazinzon</title>
+    <script src="script.js" defer type="module"></script>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+
+</body>
+</html>
