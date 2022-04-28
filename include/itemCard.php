2f5cf9a6efd7bfcad06e615b40c25a6e09f08590
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:32:11 +0200

    Placeholder Card

---
+++ include/itemCard.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <img src="https://placekitten.com/200/300" class="card-img-top" alt="...">
+    <div class="card-body">
+        test
+    </div>
+</div>
--- index.php
+++ index.php
@@ -12,13 +12,24 @@ ini_set('display_errors', 'On');
     <title>Amazinzon</title>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="script.js" defer type="module">
+    <script src="script.js" defer type="module"/>
 
     <link rel="stylesheet" href="styles.css" />
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous">
 </head>
 <body>
-
+<header class="jumbotron">
+    <div class="container">
+        <h1>Willkommen auf Amazinzon</h1>
+    </div>
+</header>
+    <section class="container" id="products">
+        <div class="row">
+            <div class="col">
+                <?php include "include/itemCard.php";?>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
