df214d99984df56bd70c6d0274f0f7355d962119
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:42:35 +0200

    Img Res Change

--- include/itemCard.php
+++ include/itemCard.php
@@ -1,5 +1,5 @@
 <div class="card">
-    <img src="https://placekitten.com/200/300" class="card-img-top" alt="...">
+    <img src="https://placekitten.com/286/180" class="card-img-top" alt="...">
     <div class="card-body">
         test
     </div>
