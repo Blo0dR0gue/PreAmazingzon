c7959cce6f6efe69920e8a1aadd8eb4f3984aa7c
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:43:49 +0200

    Missing Tag

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ ini_set('display_errors', 'On');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous">
 </head>
 <body>
+
+<header>
 <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container">
         <a href="#" class="navbar-brand d-flex align-items-center">
