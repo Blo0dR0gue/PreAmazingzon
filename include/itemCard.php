3acecdb2caf320dac76ae858427d45535b6c9e6c
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:51:14 +0200

    Header Change

--- include/itemCard.php
+++ include/itemCard.php
@@ -1,6 +1,10 @@
 <div class="card">
     <img src="https://placekitten.com/286/180" class="card-img-top" alt="...">
     <div class="card-body">
-        test
+        Super flauschig yeah
+    </div>
+    <div class="card-footer">
+        <a href="" class="btn btn-primary">Details</a>
+        <a class="btn btn-success">In den Warenkorb</a>
     </div>
 </div>
