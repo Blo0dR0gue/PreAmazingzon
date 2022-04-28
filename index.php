a78e25ecbdd8e098b407e72b0b0ec1217adb17a0
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-28 15:20:38 +0200

    Bootstrap

--- index.php
+++ index.php
@@ -10,8 +10,13 @@ ini_set('display_errors', 'On');
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Amazinzon</title>
-    <script src="script.js" defer type="module"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="script.js" defer type="module">
+
     <link rel="stylesheet" href="styles.css" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 
