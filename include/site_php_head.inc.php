aa1c85b1bf1d47dab12c1f5a8bbd8ec0076051b7
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-04-29 17:26:16 +0200

    added site_php_head.inc.php

---
+++ include/site_php_head.inc.php
@@ -0,0 +1,15 @@
+<!-- php head file included in all necessary files at the beginning -->
+
+<?php
+// php error display
+// TODO work out how reporting works
+ini_set('display_errors', 'On');
+ini_set('display_startup_errors', 'On');
+error_reporting(-1);
+
+// default includes
+// TODO
+
+// session
+//TODO
+?>
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<?php
-error_reporting(-1);
-ini_set('display_errors', 'On');
-?>
+<?php require_once "include/site_php_head.inc.php" ?>
 
 <!DOCTYPE html>
 <html class="h-100" lang="de">
