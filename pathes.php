7ba7d91697c26c33de9e4859ab1a3c1885451259
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-30 20:29:33 +0200

    Paths update

--- pathes.php
+++ pathes.php
@@ -1,18 +1,22 @@
 <?php
-const ROOT_DIR = "./";
+$dirs = explode("/", substr($_SERVER['PHP_SELF'], 1));
+$path = str_repeat("../", count($dirs) - 1);
+
+$ROOT_DIR = $path;
+
 define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-const ASSETS_DIR = ROOT_DIR . "/assets";
+define("ASSETS_DIR", $ROOT_DIR . "assets");
 const CONFIG_DIR = ASSETS_DIR . '/config';
 const FILE_DIR = ASSETS_DIR . '/files';
 const IMAGE_DIR = ASSETS_DIR . '/images';
 const SCRIPT_DIR = ASSETS_DIR . '/scripts';
 const STYLE_DIR = ASSETS_DIR . '/styles';
 
-const CONTROLLER_DIR = ROOT_DIR . 'controller';
+define("CONTROLLER_DIR", $ROOT_DIR . 'controller');
 
-const INCLUDE_DIR = ROOT_DIR . 'include';
+define("INCLUDE_DIR", $ROOT_DIR . 'include');
 
-const MODEL_DIR = ROOT_DIR . 'model';
+define("MODEL_DIR", $ROOT_DIR . 'model');
 
-const PAGE_DIR = ROOT_DIR . 'pages';
+define("PAGE_DIR", $ROOT_DIR . 'pages');
