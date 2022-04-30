2b2bbeda54d55d52f6d0570c97e0e9ea6448ba9d
Daniel Czeschner <daniel.czeschner@web.de>
2022-04-30 21:50:46 +0200

    Database added

---
+++ assets/config/database.php
@@ -0,0 +1,6 @@
+<?php
+const DB_DATABASE = 'amazingzon';
+const DB_USERNAME = 'amazingzon';
+const DB_PASSWORD = '';
+const DB_HOST = '127.0.0.1';
+const DB_PORT = '3306';
---
+++ include/database.inc.php
@@ -0,0 +1,12 @@
+<?php
+function getDB(): mysqli
+{
+    static $db;
+    if ($db instanceof MYSQLI) {
+        return $db;
+    }
+
+    require_once CONFIG_DIR . '/database.php';
+    $db = new MYSQLI(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+    return $db;
+}
