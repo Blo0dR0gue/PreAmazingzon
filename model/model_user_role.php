2f7af815514526420b9c130defb22812bec74431
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-02 19:59:14 +0200

    worked on model_user_role.php

--- model/model_user_role.php
+++ model/model_user_role.php
@@ -4,17 +4,59 @@
 // load required files
 require_once(INCLUDE_DIR . DIRECTORY_SEPARATOR . "database.inc.php");
 
-// TODO implement
 class UserRole
 {
     // region fields
-
+    private int $id;
+    private string $name;
     // endregion
 
+    /**
+     * Constructor for UserRole.
+     * @param int $id
+     * @param string $name
+     */
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
 
     // region getter
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
     // endregion
 
+    /**
+     * Get an existing UserRole by its id.
+     * @param int $id ID of UserRole
+     * @return UserRole found UserRole
+     */
+    public static function getById(int $id): UserRole
+    {
+        // TODO ERROR handling
+        $stmt = getDB()->prepare("SELECT * from userrole where id = ?;");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+
+        $res = $stmt->get_result()->fetch_assoc();
+
+        $stmt->close();
+
+        return new UserRole($id, $res['name']);
+    }
 }
 ?>
