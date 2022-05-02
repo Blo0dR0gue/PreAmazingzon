ac17cefb4ae93211162e1a93a79918b42d7ded2b
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-02 18:49:08 +0200

    Closing-Tag missing

--- include/elem_item_card.inc.php
+++ include/elem_item_card.inc.php
@@ -2,12 +2,14 @@
 if (isset($product)):
 ?>
     <div class="card">
-        <div class="card-title"><?= $product['title'] ?></div>
+        <div class="card-title">
+            <?= $product['title'] ?>
+        </div>
         <img src="<?= $product['mainImg'] ?>" class="card-img-top" alt="product">
         <div class="card-body">
             <?= $product['description'] ?>
             <hr>
-            <strong><?= $product['price'] ?> €</strong
+            <strong><?= $product['price'] ?> €</strong>
         </div>
         <div class="card-footer">
             <a href="#" class="btn btn-primary btn-sm">Details</a>
