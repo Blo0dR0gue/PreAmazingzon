99398e932a88fe7ec5c994061d7e08da3afaa262
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-02 19:46:42 +0200

    added structure for all other needed models

---
+++ model/model_review.php
@@ -0,0 +1,34 @@
+<!-- TODO COMMENT-->
+
+<?php
+// load required files
+require_once(INCLUDE_DIR . DIRECTORY_SEPARATOR . "database.inc.php");
+
+// TODO implement
+class Review
+{
+    // region fields
+
+    // endregion
+
+
+    // region getter
+
+    // endregion
+
+    public function insert(): void
+    {
+        // TODO
+    }
+
+    public function update(): void
+    {
+        // TODO
+    }
+
+    public function delete(): void
+    {
+        // TODO
+    }
+}
+?>
