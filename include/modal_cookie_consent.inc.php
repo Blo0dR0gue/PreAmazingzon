e1cf7448f02fd45183953961d5229517542f2aa0
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-05 11:32:38 +0200

    added centered cookie-modal

--- include/modal_cookie_consent.inc.php
+++ include/modal_cookie_consent.inc.php
@@ -3,7 +3,7 @@
 <?php if (!isset($_COOKIE["cookie_consent"])) { ?>
 <!-- cookie modal -->
 <div class="modal fade d-block py-5" tabindex="-1" id="modalCookie">
-    <div class="modal-dialog py-5" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content rounded-6 shadow">
             <div class="modal-header border-bottom-0">
                 <h4 class="modal-title">Cookie Consent Request</h4>
@@ -14,7 +14,7 @@
                     of cookies to continue to use the services of the website.</p>
             </div>
             <div class="modal-footer flex-column border-top-0">
-                <button type="button" class="btn btn-lg btn-primary w-100 mx-0 mb-2" onclick="acceptCookies()" >Accept</button>
+                <button type="button" class="btn btn-lg btn-primary w-100 mx-0 mb-2" data-dismiss="modal" onclick="acceptCookies()" >Accept</button>
                 <button type="button" class="btn btn-lg btn-light w-100 mx-0" onclick="declineCookies()">Decline</button>
             </div>
         </div>
