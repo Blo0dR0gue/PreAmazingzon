e82777a7c8b6e74d1fc5348ffd253a8ae71d3d69
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-05 13:41:39 +0200

    Added FontAwesome and custom rating css

--- include/elem_item_card.inc.php
+++ include/elem_item_card.inc.php
@@ -1,15 +1,24 @@
 <?php
 if (isset($product) && $product instanceof Product):
-?>
+    ?>
     <div class="card">
         <img src="<?= $product->getMainImg(); ?>" class="card-img-top" alt="product">
         <div class="card-body">
-            <?= $product->getTitle(); ?>
+
+            <a href="<?= PAGES_DIR . DIRECTORY_SEPARATOR . 'page_product_detail.php?id=' . $product->getId(); ?>"
+               data-abc="true" style="text-decoration: none"><?= $product->getTitle(); ?></a>
             <hr>
             <strong><?= $product->getPriceFormatted(); ?> €</strong>
+            <hr>
+            <div class="ratings">
+                <i class="fa fa-star rating-color"></i>
+                <i class="fa fa-star rating-color"></i>
+                <i class="fa fa-star rating-color"></i>
+                <i class="fa fa-star rating-color"></i>
+                <i class="fa fa-star"></i>
+            </div>
         </div>
         <div class="card-footer">
-            <a href="#" class="btn btn-primary btn-sm">Details</a>
             <a href="#" class="btn btn-success btn-sm">Add to cart</a>
         </div>
     </div>
