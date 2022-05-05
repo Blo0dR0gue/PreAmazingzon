528f14b6b0e5f88138282c1ab7ae0bdbeeb0bdf3
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-05 19:44:34 +0200

    added controller_user.php

---
+++ controller/controller_user.php
@@ -0,0 +1,32 @@
+<!--TODO Comments-->
+
+<?php
+
+require_once MODEL_DIR . DIRECTORY_SEPARATOR . "model_user.php";
+
+class UserController
+{
+
+    public static function getByEmail(string $email): ?User
+    {   // TODO validate
+        return User::getByEmail($email);
+    }
+
+    public static function login(User $user, string $password): bool
+    {   // TODO validate
+
+        if ($user->isActive()){     // only login active users
+            if (password_verify($password, $user->getPasswordHash()))   // valid credentials?
+            {
+                // save the login state
+                $_SESSION['login'] = true;
+                $_SESSION['user'] = $user;
+
+                return true;    // success
+            }
+        }
+
+        return false;   // failure
+    }
+
+}
