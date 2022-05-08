513d8cee36f2befe57abed313f433be2f5285346
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-08 18:37:18 +0200

    Moved Avg Star Rating into controller

--- controller/controller_review.php
+++ controller/controller_review.php
@@ -19,4 +19,28 @@ class ReviewController
 
     }
 
+    /**
+     * Calculate and set the star rating using full and half stars.
+     * @param int $productID The ProductID, for which the Stars should be included.
+     * @return void HTML-Tags
+     */
+    public static function calcAndIncAvgProductStars(int $productID): void
+    {
+        $avgRating = self::getAvgRating($productID);
+
+        for ($i = 1; $i <= 5; $i++)
+        {
+            $difference = $avgRating - $i;
+            if ($difference >= 0)
+            {
+                echo "<i class='fa fa-star rating-color'></i>";     // full star
+            } elseif (-0.25 > $difference && $difference > -0.75)
+            {
+                echo "<i class='fa fa-star-half-full rating-color'></i>";   // half star
+            } else {
+                echo "<i class='fa fa-star-o rating-color'></i>";      // empty star
+            }
+        }
+    }
+
 }
