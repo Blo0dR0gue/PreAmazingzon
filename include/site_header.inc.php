b89040668539e670e070a1bcd90c0d62f7f1b40c
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-05-09 16:50:52 +0200

    cleanup; rename elem_product_card.inc.php

--- include/site_header.inc.php
+++ include/site_header.inc.php
@@ -5,13 +5,15 @@
 <header class="p-3 bg-dark text-white">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="<?= ROOT_DIR ?>" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <!-- logo -->
+            <a href="<?= ROOT_DIR ?>" class="d-flex align-items-center mb-2 mb-lg-0">
                 <img src="<?= IMAGE_LOGO_DIR . DIRECTORY_SEPARATOR . "logo_long_inv.svg" ?>" class="bi me-2" width="150" height="40"
                      alt="Company Logo">
             </a>
 
+            <!-- nav -->
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="<?= ROOT_DIR ?>" class="nav-link px-2 text-secondary">Home</a></li>
+                <li><a href="<?= ROOT_DIR ?>" class="nav-link px-2 text-white">Home</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
                 <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
@@ -19,12 +21,13 @@
                        class="nav-link px-2 text-white">About</a></li>
             </ul>
 
+            <!-- search -->
 <!--            TODO make search work -->
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                 <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
             </form>
 
-            <!-- defendant on login state-->
+            <!-- dependant on login state-->
             <?php if (!isset($_SESSION["login"])) { ?>
                 <!-- show login and register buttons -->
                 <div class="text-end">
@@ -37,7 +40,7 @@
                 <!-- show profile action -->
                 <!-- TODO add links-->
                 <div class="dropdown text-end">
-                    <a href="#" class="d-block link-light text-decoration-none dropdown-toggle" id="dropdownUser"
+                    <a href="" class="d-block link-light text-decoration-none dropdown-toggle" id="dropdownUser"
                        data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="<?= IMAGE_DIR . DIRECTORY_SEPARATOR . "user_orange.svg" ?>" alt="mdo"
                              class="rounded-circle" width="40" height="40">
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             require CONTROLLER_DIR . DIRECTORY_SEPARATOR . "controller_product.php";
             foreach (ProductController::getRandomProducts(INDEX_PRODUCTS_AMOUNT) as $product): ?>
                 <div class="col-3">
-                    <?php require INCLUDE_DIR . DIRECTORY_SEPARATOR . "elem_item_card.inc.php"; ?>
+                    <?php require INCLUDE_DIR . DIRECTORY_SEPARATOR . "elem_product_card.inc.php"; ?>
                 </div>
             <?php endforeach; ?>
         </div>
