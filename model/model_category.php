aca931df31e2b26738a093dd4b557933560fab1a
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-09 20:53:46 +0200

    Worked on category model

--- model/model_category.php
+++ model/model_category.php
@@ -9,10 +9,47 @@ class Category
 {
     // region fields
 
+    private int $id;
+    private string $name;
+    private string $description;
+    private int $parentID;
+
     // endregion
 
 
     // region getter
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getParentID(): int
+    {
+        return $this->parentID;
+    }
 
     // endregion
 
