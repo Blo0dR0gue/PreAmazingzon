3ac46e6d1ba83453fd08c7872a3126fa6276926d
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-14 21:52:22 +0200

    Moved page script to separate file

--- pages/admin/page_add_product.php
+++ pages/admin/page_add_product.php
@@ -209,6 +209,8 @@ if (isset($_POST["title"]) && isset($_POST["cat"]) && isset($_POST["description"
 <script src="<?= SCRIPT_DIR . DIRECTORY_SEPARATOR . "form_validation.js" ?>"></script>
 <!-- enable tooltips on this page (by default disabled for performance)-->
 <script src="<?= SCRIPT_DIR . DIRECTORY_SEPARATOR . "tooltip_enable.js" ?>"></script>
+<!-- enable admin page tools-->
+<script src="<?= SCRIPT_DIR . DIRECTORY_SEPARATOR . "admin_pages.js" ?>"></script>
 
 <!-- show error popup -->
 <?php
@@ -220,98 +222,6 @@ if (isset($processingError)) // processing error
     );
 }
 ?>
-
-<script>
-    $(function () {
-        $("#dropZone").click(e => {
-            if (e.target === $("#dropZone")[0] || e.target === $("#imgRow")[0])   //Only, if we click the div and not a sub element.
-                $("#files").click();    //Open the file explorer
-        });
-    });
-
-    function dropHandler(ev, maxFileAmount) {
-        ev.preventDefault();
-
-        if (ev.dataTransfer.items) {
-            // Use DataTransferItemList interface to access the file(s)
-            for (let i = 0; i < ev.dataTransfer.items.length; i++) {
-                // If dropped items aren't files, reject them
-                if (ev.dataTransfer.items[i].kind === 'file') {
-                    var file = ev.dataTransfer.items[i].getAsFile();
-                    addImg(file, maxFileAmount);
-                }
-            }
-        } else {
-            // Use DataTransfer interface to access the file(s)
-            for (let i = 0; i < ev.dataTransfer.files.length; i++) {
-                addImg(ev.dataTransfer.files[i], maxFileAmount);
-            }
-        }
-    }
-
-    function dragOverHandler(ev) {
-        ev.preventDefault();
-    }
-
-    function getTemplate(templateSelector) {
-        return document.querySelector(templateSelector).content;
-    }
-
-    const FILES = new Map();
-    let imgID = 0;
-    const main = getTemplate("#imgBoxTemplate");
-
-    //JQuery does not have a formdata event :(
-    document.getElementById("prodForm").addEventListener('formdata', (e) => {
-        const formData = e.formData;
-
-        formData.delete('files[]');
-
-        FILES.forEach(function (value) {
-            formData.append("files[]", value, value.name);
-        });
-    })
-
-    function addImg(file, maxFileAmount) {
-        if (!file || file['type'].split('/')[0] !== 'image') return;
-        if(FILES.size >= maxFileAmount) return;
-        $("#dropTexts")[0].style.display = "none";
-        let template = main.cloneNode(true);
-
-        template.querySelector('div img').src = URL.createObjectURL(file);
-        template.querySelector('div button').dataset.id = imgID.toString();
-
-        FILES.set(imgID, file);
-        imgID++;
-
-        $("#imgRow")[0].appendChild(template)
-    }
-
-    function filesChanged(fileElem, maxFileAmount) {
-        for (let i = 0; i < fileElem.files.length; i++) {
-            let file = fileElem.files[i];
-            addImg(file, maxFileAmount);
-        }
-    }
-
-    function deleteImg(imgElem) {
-        const _imgID = parseInt(imgElem.dataset.id);
-
-        const imgBox = imgElem.parentElement;
-        const imgContainer = imgBox.parentElement;
-        imgContainer.removeChild(imgBox);
-
-        FILES.delete(_imgID);
-
-        if (FILES.size === 0)
-            $("#dropTexts")[0].style.display = "block";
-    }
-
-    function handleRadioUpdate(myRadio) {
-        document.getElementById("selectedRadio").value = myRadio.dataset.name;
-    }
-
-</script>
 
 </body>
 </html>
