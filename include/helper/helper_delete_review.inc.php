7abfe4fe1a3a080bec0cfb30f493a6ff293f6ae2
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-21 08:05:27 +0200

    redirect back if possible

--- include/helper/helper_delete_review.inc.php
+++ include/helper/helper_delete_review.inc.php
@@ -6,23 +6,44 @@ require_once CONTROLLER_DIR . DS . "controller_review.php";
 
 UserController::redirectIfNotAdmin();
 
-if(!isset($_GET["productId"]) || !is_numeric($_GET["productId"])){
-    header("LOCATION: " . ROOT_DIR);
+if (!isset($_GET["productId"]) || !is_numeric($_GET["productId"])) {
+    //Go back to previous page, if it got set, else to the index.php
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("LOCATION: " . ROOT_DIR);
+    }
     die();
 }
 
-if(!isset($_GET["id"]) || !is_numeric($_GET["id"])){
-    header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=".$_GET["productId"]);
+if (!isset($_GET["id"]) || !is_numeric($_GET["id"])) {
+    //Go back to previous page, if it got set, else go back to the page_product_detail.php page
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]); //In this way, we can keep all set get parameters in the url
+    } else {
+        header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=" . $_GET["productId"]);
+    }
     die();
 }
 
 $review = ReviewController::getById($_GET["id"]);
 
-if(!isset($review)){
-    header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=".$_GET["productId"]);
+if (!isset($review)) {
+    //Go back to previous page, if it got set, else go back to the page_product_detail.php page
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]); //In this way, we can keep all set get parameters in the url
+    } else {
+        header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=" . $_GET["productId"]);
+    }
     die();
 }
 
 $suc = ReviewController::delete($review);
 
-header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=".$_GET["productId"]);
+//Go back to previous page, if it got set, else go back to the page_product_detail.php page
+if (isset($_SERVER["HTTP_REFERER"])) {
+    header("Location: " . $_SERVER["HTTP_REFERER"]); //In this way, we can keep all set get parameters in the url
+} else {
+    header("Location: " . PAGES_DIR . DS . "page_product_detail.php?id=" . $_GET["productId"]);
+}
+die();
