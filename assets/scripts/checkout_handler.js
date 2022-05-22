4bef9cf2e0c0116746142a198784537af46e2582
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-22 14:22:12 +0200

    comment

--- assets/scripts/checkout_handler.js
+++ assets/scripts/checkout_handler.js
@@ -1,10 +1,12 @@
 $(function(){
-
+    //Check if the delivery address radio select has changed.
     $("#checkoutForm input[type=radio][name='delivery']").on("change", function() {
+        //Update the visuals of the selected address
         const dataSet = $("input[name='delivery']:checked").data();
         $("#selectedDeliveryName").text(dataSet["user"]);
         $("#selectedDeliveryStreet").text(dataSet["street"]);
         $("#selectedDeliveryCity").text(dataSet["city"]);
+        //Disable the no address text
         $("#noDeliveryText").css("display", "none");
     });
 
