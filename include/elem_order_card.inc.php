72b9b2378afb43300280b728597d0ef17c460cfb
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-23 21:19:32 +0200

    style update

--- include/elem_order_card.inc.php
+++ include/elem_order_card.inc.php
@@ -1,24 +1,29 @@
 <?php if (isset($order) && $order instanceof Order): ?>
     <div class="callout mb-5 border">
-        <h5 class="mb-1">Order
-            number <?= $order->getId() . " (" . OrderStateController::getById($order->getOrderStateId())->getLabel() . ")" ?></h5>
-        <p class="mb-2">Order Date: <?= $order->getFormattedOrderDate(); ?></p>
-        <p class="mb-2">Delivery Date: <?= $order->getFormattedDeliveryDate(); ?></p>
+        <h5 class="mb-2">Order
+            number <?= $order->getId() . " (Status: " . OrderStateController::getById($order->getOrderStateId())->getLabel() . ")" ?></h5>
+        <p class="mb-0">Paid: <?= $order->isPaid()?"Paid":"Not Paid" ?></p>
+        <p class="mb-0">Order Date: <?= $order->getFormattedOrderDate(); ?></p>
+        <p class="mb-0">Delivery Date: <?= $order->getFormattedDeliveryDate(); ?></p>
         <hr>
         <?php
 
+        //Total price for all products
         $sum = 0;
+        //Total count of all products
         $count = 0;
+        //Get all products which got ordered
         $productOrders = ProductOrderController::getAllByOrder($order->getId());
 
         foreach ($productOrders as $orderItem) {
+            //Get the product object for the current order product
             $product = ProductController::getByID($orderItem->getProductId());
             ?>
             <div class="row mb-3">
                 <div class="col-6 col-md-2">
-
+                    <!--Image-->
                     <a href="<?= isset($product) ? PAGES_DIR . DS . "page_product_detail.php?id=" . $product->getId() : "#";
-                    // TODO GLOBAL dont use manual queries, http_build_query instead  ?>"
+                    // TODO GLOBAL dont use manual queries, http_build_query instead   ?>"
                        class="d-flex justify-content-center align-items-center overflow-hidden">
                         <img src="<?= isset($product) ? $product->getMainImg() : IMAGE_PRODUCT_DIR . DS . "notfound.jpg"; ?>"
                              width="90"
@@ -26,6 +31,7 @@
                     </a>
 
                 </div>
+                <!--Title-->
                 <div class="col-6 col-md-4 d-flex align-items-center justify-content-center">
                     <a href="<?= isset($product) ? PAGES_DIR . DS . "page_product_detail.php?id=" . $product->getId() : "#"; ?>"
                        class="mb-0 h5 text-decoration-none text-black web"
@@ -33,23 +39,31 @@
                         <?= isset($product) ? $product->getTitle() : "Product not found" ?>
                     </a>
                 </div>
+
+                <!--Space-->
                 <div class="d-sm-none d-md-block col-md-2">
 
                 </div>
+
+                <!--Amount-->
                 <div class="col-6 col-md-2 d-flex align-items-center">
                     <?php
                     $count += $orderItem->getAmount();
                     echo $orderItem->getAmount() ?> pcs.
                 </div>
+
+                <!--Price-->
                 <div class="col-6 col-md-2 d-flex align-items-center">
                     <?php
                     $sum += $orderItem->getAmount() * $orderItem->getPrice();
-                    echo number_format($orderItem->getAmount() * $orderItem->getPrice(), 2, ",", ""); ?> €
+                    echo number_format($orderItem->getAmount() * $orderItem->getPrice(), 2, ",", "") . CURRENCY_SYMBOL; ?>
                 </div>
             </div>
         <?php } ?>
+
         <hr>
-        <!-- Total: -->
+
+        <!-- Total Stats -->
         <div class="row mb-3">
             <div class="d-sm-none d-md-block col-md-6">
 
@@ -64,23 +78,24 @@
                 <h6><?= number_format($sum, 2, ",", "") . CURRENCY_SYMBOL; ?> </h6>
             </div>
         </div>
+
         <!-- Invoice -->
         <div class="row">
             <div class="d-sm-none col-md-6">
 
             </div>
-            <div class="d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center">
                 <div class="col-6">
-                    <a class="fs-6"
-                       href="#">Download invoice</a>    <!--TODO-->
+                    <button class="btn btn-sm fs-6"
+                       href="#">Download invoice</button>    <!--TODO-->
                 </div>
 
                 <?php if (!$order->isPaid()) { ?>
-                    <button class="btn btn-primary col-6" onclick="#">
+                    <button class="btn btn-warning col-4 btn-sm" onclick="#">
                         Pay <!--TODO-->
                     </button>
                 <?php } else { ?>
-                    <div class="col-6 alert alert-success text-center mb-0">
+                    <div class="col-4 alert alert-success text-center mb-0">
                         Paid
                     </div>
                 <?php } ?>
