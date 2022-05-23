ade397d2cf9099d1cdac8423b69f430b48bbd04c
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-23 19:53:18 +0200

    added order page

---
+++ pages/user/orders.php
@@ -0,0 +1,26 @@
+<!-- TODO COMMENT -->
+<?php require_once "../include/site_php_head.inc.php" ?>
+
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+<head>
+    <?php require_once INCLUDE_DIR . DS . "site_html_head.inc.php"; ?>
+    <title><?= PAGE_NAME ?> - Orders</title>
+</head>
+
+<body class="d-flex flex-column h-100">
+<!-- header -->
+<?php require_once INCLUDE_DIR . DS . "site_header.inc.php"; ?>
+
+<!-- main body -->
+<main class="flex-shrink-0">
+
+
+
+</main>
+
+<!-- footer -->
+<?php require_once INCLUDE_DIR . DS . "site_footer.inc.php"; ?>
+
+</body>
+</html>
