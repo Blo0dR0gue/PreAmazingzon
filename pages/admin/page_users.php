ea576c34013c2518c4fc7fb263322bb539aaf088
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-26 11:30:52 +0200

    Added needed files (TODO)

---
+++ pages/admin/page_users.php
@@ -0,0 +1,24 @@
+<!-- TODO COMMENT -->
+<?php require_once "../../include/site_php_head.inc.php" ?>
+
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+<head>
+    <?php require_once INCLUDE_DIR . DS . "site_html_head.inc.php"; ?>
+    <title><?= PAGE_NAME ?> - Admin - Users</title>
+</head>
+
+<body class="d-flex flex-column h-100">
+<!-- header -->
+<?php require_once INCLUDE_DIR . DS . "site_header.inc.php"; ?>
+
+<!-- main body -->
+<main class="flex-shrink-0">
+    <!--TODO make handle users -->
+</main>
+
+<!-- footer -->
+<?php require_once INCLUDE_DIR . DS . "site_footer.inc.php"; ?>
+
+</body>
+</html>
