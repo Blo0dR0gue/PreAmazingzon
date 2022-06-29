fc93f27ae540055b98a667bb590995f76b878295
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-29 16:27:33 +0200

    Added delete user via admin panel

---
+++ include/helper/helper_delete_user.inc.php
@@ -0,0 +1,37 @@
+<?php
+//Deletes a user
+require_once "../site_php_head.inc.php";
+
+UserController::redirectIfNotAdmin();
+
+if (!isset($_GET["id"]) || !is_numeric($_GET["id"])) {
+    //Go back to previous page, if it got set, else to the index.php
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    } else {
+        header("LOCATION: " . ROOT_DIR);
+    }
+    die();
+}
+
+$user = UserController::getById($_GET["id"]);
+
+if (!isset($user)) {
+    //Go back to previous page, if it got set, else go back to the page_users.php page
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]); //In this way, we can keep all set get parameters in the url
+    } else {
+        header("Location: " . ADMIN_PAGES_DIR . "page_users.php");
+    }
+    die();
+}
+
+$suc = UserController::delete($user);
+
+//Go back to previous page, if it got set, else go back to the page_users.php page
+if (isset($_SERVER["HTTP_REFERER"])) {
+    header("Location: " . $_SERVER["HTTP_REFERER"]); //In this way, we can keep all set get parameters in the url
+} else {
+    header("Location: " . ADMIN_PAGES_DIR . "page_users.php");
+}
+die();
