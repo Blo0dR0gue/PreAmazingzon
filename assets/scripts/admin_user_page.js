d413cb44a7d8c7c318d5a8301dfba119176c022e
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-30 19:39:42 +0200

    log status

--- assets/scripts/admin_user_page.js
+++ assets/scripts/admin_user_page.js
@@ -8,14 +8,16 @@ function onToggleUserActivation(button, userId) {
         success: function (result) {
             const response_data = $.parseJSON(result);
 
-            if (response_data.msg === "done") {
+            if (response_data.state === "success") {
 
                 button.classList.toggle("btn-success");
                 button.classList.toggle("btn-warning");
 
                 $("td[data-id=" + userId + "]").text(response_data.active ? "Yes" : "No");
+                console.log(response_data.msg);
+            }else if(response_data.state === "error"){
+                console.log(response_data.msg);
             }
-            //TODO error handling
         }
     });
 }
--- include/helper/helper_toggle_user.inc.php
+++ include/helper/helper_toggle_user.inc.php
@@ -6,21 +6,21 @@ require_once "../site_php_head.inc.php";
 UserController::redirectIfNotAdmin();
 
 if (!isset($_POST["userId"]) || !is_numeric($_POST["userId"])) {
-    echo json_encode(array("msg" => "value error"));
+    echo json_encode(array("state" => "success", "msg" => "value error"));
     die();
 }
 
 $user = UserController::getById($_POST["userId"]);
 
 if (!isset($user)) {
-    exit(json_encode(array("msg" => "user error")));
+    exit(json_encode(array("state" => "success", "msg" => "user error")));
 }
 
 $user->setActive(!$user->isActive());
 $user = $user->update();
 
 if (!isset($user)) {
-    exit(json_encode(array("msg" => "update error")));
+    exit(json_encode(array("state" => "success", "msg" => "update error")));
 }
 
-exit(json_encode(array("msg" => "done", "active" => $user->isActive())));
+exit(json_encode(array("state" => "success", "msg" => "done", "active" => $user->isActive())));
