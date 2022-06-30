6b3491d5b9c57d7357e8657d64083725102fd19c
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-06-30 21:30:08 +0200

    worked on category infrastructure

---
+++ include/admin/admin_category_add_edit.inc.php
@@ -0,0 +1,77 @@
+<!-- TODO comment -->
+
+<form action="" id="catForm" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
+    <div class="card my-4">
+        <!-- HEADER -->
+        <div class="card-header">
+            <!-- title -->
+            <h2 class="mb-2 mt-4">
+                <?php
+                if (str_contains($_SERVER["REQUEST_URI"], "add")) {
+                    echo "Add Category";
+                } elseif (str_contains($_SERVER["REQUEST_URI"], "edit")) {
+                    echo "Edit Category";
+                } else {
+                    echo "Category";
+                }
+                ?>
+            </h2>
+        </div>
+
+        <!-- BODY -->
+        <div class="card-body">
+            <!-- category title -->
+            <div class="form-group position-relative mb-1">
+                <label for="title">Category Title</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="A Category Title"
+                       value="<?php if (isset($category) && $category instanceof Category) { echo $category->getName(); } ?>"
+                       required pattern="[a-zäöüA-ZÄÖÜ0-9 ,.'-:]{5,}">
+                <div class="invalid-tooltip opacity-75">Please enter a valid category name!</div>
+            </div>
+
+            <!-- super category select -->
+            <div class="form-group position-relative mb-1">
+                <label for="selectedRadio">Super Category</label>
+                <select class="form-select" name="cat" required>
+                    <!-- TODO Rework -> tree like?; Replace button next with selected -->
+                    <option value="" hidden>Select Super Category</option>
+
+                    <?php foreach (CategoryController::getCategoryTree() as $treeEntry): ?>
+                        <option value="<?= $treeEntry["top"]; ?>"
+                            <?php
+                            if (isset($cat)) {
+                                if (in_array($treeEntry["top"], $cat)) { echo "selected"; }
+                            } else if (isset($category) && $category instanceof Category) {
+                                if ($treeEntry["top"] == $category->getParentID()) { echo "selected"; } // TODO doesnt work for root?
+                            } ?>>
+                            <?= $treeEntry["path"]; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+                <div class="invalid-tooltip opacity-75">Please select a super category!</div>
+            </div>
+
+            <!-- category description (text) -->
+            <div class="form-group position-relative mb-1">
+                <label for="description">Category Description</label>
+                <!-- textarea value MUST be in one line, hence the placeholder does not work -->
+                <textarea class="form-control" id="description" name="description" placeholder="My Category" required
+                          rows="3"><?php if (isset($category) && $category instanceof Category) { echo $category->getDescription(); } ?></textarea>
+                <div class="invalid-tooltip opacity-75">Please add a category description!</div>
+            </div>
+        </div>
+
+        <!-- FOOTER -->
+        <div class="card-footer">
+            <a href="javascript:history.back()" class="btn btn-secondary">Cancel</a>
+            <button class="btn btn-success">Save</button>
+        </div>
+    </div>
+</form>
+
+<!-- load custom form validation script -->
+<script src="<?= SCRIPT_DIR . "form_validation.js" ?>"></script>
+<!-- enable tooltips on this page (by default disabled for performance) -->
+<script src="<?= SCRIPT_DIR . "tooltip_enable.js" ?>"></script>
+<!-- enable admin page tools -->
+<script src="<?= SCRIPT_DIR . "admin_pages.js" ?>"></script>
---
+++ pages/admin/page_category_add.php
@@ -0,0 +1,53 @@
+<!-- TODO Comment -->
+
+<?php
+require_once "../../include/site_php_head.inc.php";
+
+UserController::redirectIfNotAdmin();   // User is not allowed to be here.
+
+$isPost = strtolower($_SERVER["REQUEST_METHOD"]) === "post";
+
+if (isset($_POST["title"]) && isset($_POST["cat"]) && isset($_POST["description"]) && $isPost) {
+
+    $category = CategoryController::insert(
+        $_POST["title"],
+        $_POST["cat"],
+        $_POST["description"]
+    );
+
+    if (isset($category)) {
+        header("LOCATION: " . ADMIN_PAGES_DIR . 'page_categories.php');  // go to admin categories page
+        // TODO success msg?
+        die();
+    }
+    $processingError = true;
+}
+?>
+
+<!DOCTYPE html>
+<html class="h-100" lang="en">
+<head>
+    <?php
+    require_once INCLUDE_DIR . "site_html_head.inc.php";
+    require_once INCLUDE_DIR . "modal_popup.inc.php";
+    ?>
+    <title><?= PAGE_NAME ?> - Admin - Category - Add</title>
+
+    <!-- file specific includes-->
+    <link rel="stylesheet" href="<?= STYLE_DIR . "style_admin_pages.css"; ?>">
+</head>
+
+<body class="d-flex flex-column h-100">
+<!-- header -->
+<?php require_once INCLUDE_DIR . "site_header.inc.php"; ?>
+
+<!-- main body -->
+<main class="m-auto w-100 px-3" style="max-width: 800px">
+    <?php require_once INCLUDE_ADMIN_DIR . "admin_category_add_edit.inc.php"; ?>
+</main>
+
+<!-- footer -->
+<?php require_once INCLUDE_DIR . "site_footer.inc.php" ?>
+
+</body>
+</html>
