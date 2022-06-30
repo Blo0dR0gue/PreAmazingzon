a8cf4c297a29e115b4bf44bbcbccd1963c26a604
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-30 19:43:00 +0200

    pay order files

---
+++ assets/scripts/user_order_page.js
@@ -0,0 +1,10 @@
+/**
+ * Needs to be replaced with an actual pay function.
+ * @param elem
+ * @param orderId
+ */
+function onItemPayBtn(elem, orderId){
+
+
+
+}
---
+++ include/helper/helper_pay_order.inc.php
@@ -0,0 +1,2 @@
+<?php
+//Pays an order
