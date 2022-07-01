3c45407962e7249d4c623c7ca2ab5bb01c3c6ffd
Daniel Czeschner <daniel.czeschner@web.de>
2022-07-01 20:01:58 +0200

    Comments

--- controller/controller_category.php
+++ controller/controller_category.php
@@ -5,27 +5,52 @@ require_once MODEL_DIR . 'model_category.php';
 
 class CategoryController
 {
+
+    /**
+     * Get all {@link Category}s
+     * @return array A array with all {@link Category}s.
+     */
     public static function getAll(): array
     {
         return Category::getAll();
     }
 
+    /**
+     * Gets a name of a category by its id.
+     * @param int|null $id The id.
+     * @return string The name of the category. (Can be empty if no category got found)
+     */
     public static function getNameById(?int $id): string
     {
         if (!isset($id)) { return "No Category"; }
         return self::getById($id)->getName();
     }
 
+    /**
+     * Gets a {@link Category} by its id.
+     * @param int|null $id The id.
+     * @return Category|null The {@link Category} object or null, if not found.
+     */
     public static function getById(?int $id): ?Category
     {
         return Category::getById($id);
     }
 
+    /**
+     * Gets a {@link Category} by its name.
+     * @param string $name The name.
+     * @return Category|null The {@link Category} object or null, if not found.
+     */
     public static function getByName(string $name): ?Category
     {
         return Category::getByName($name); //TODO error handling; validation
     }
 
+    /**
+     * Gets all subcategories for a {@link Category}.
+     * @param int $superId The id of the category.
+     * @return array|null A array with all sub {@link Category} objects or null, if an error occurred.
+     */
     public static function getSubCategories(int $superId): ?array
     {
         if($superId != -1){
@@ -35,12 +60,22 @@ class CategoryController
         }
     }
 
+    /**
+     * Gets the full path to a category for a product.
+     * @param Product $product The {@link Product}.
+     * @return string The category path.
+     */
     public static function getCategoryPathAsString(Product $product): string    // TODO not used?
     {
         return implode(" > ", self::getCategoryPathByProduct($product));
     }
 
-    public static function getCategoryPathByProduct(Product $product): array    // TODO not used?
+    /**
+     * Gets the full path to a category for a product.
+     * @param Product $product The {@link Product}
+     * @return array A array with each {@link Category}. Root category is on index 0.
+     */
+    public static function getCategoryPathByProduct(Product $product): array
     {
         $tmpProductCat = Category::getById($product->getCategoryID());
 
@@ -55,17 +90,36 @@ class CategoryController
         return array_reverse($path);
     }
 
+    /**
+     * Gets a specific amount categories from an offset out of the database.
+     * @param int $offset The offset from where to select.
+     * @param int $amount The amount of categories, which should be selected.
+     * @return array
+     */
     public static function getCategoriesInRange(int $offset = 0, int $amount = 8): array
     {
         return Category::getCategoriesInRange($offset, $amount);
     }
 
+    /**
+     * Gets a path to a specific category
+     * @param int|null $categoryID The id of the category
+     * @return string The path to this category. From root to $categoryID
+     */
     public static function getPathToCategory(?int $categoryID): string
     {
         if (!isset($categoryID)) { return ""; }
         return Category::getPathToCategory($categoryID);
     }
 
+    /**
+     * Gets the full tree for all categories. <br>
+     * Each entry has the key "root", "top" and "path" <br>
+     * "root" is the lowest point of the subtree. <br>
+     * "top" is the head of the subtree. <br>
+     * "path" is the full path for this subtree.
+     * @return array An array witch each subtree.
+     */
     public static function getCategoryTree(): array
     {
         return Category::getCategoryTree();
@@ -81,7 +135,15 @@ class CategoryController
         return Category::getAmountOfCategories($searchFilter);
     }
 
-    public static function update(Category $category, string $title, string $description, int $parent): ?Category
+    /**
+     * Updates an {@link Category} object
+     * @param Category $category The {@link Category} object, which should be updated.
+     * @param string $title The new title.
+     * @param string $description The new description.
+     * @param int|null $parent The new parent category.
+     * @return Category|null The updated {@link Category} or null, if an error occurred.
+     */
+    public static function update(Category $category, string $title, string $description, ?int $parent): ?Category
     {
         $category->setName(htmlspecialchars($title, ENT_QUOTES, 'UTF-8'));
         $category->setDescription(htmlspecialchars($description, ENT_QUOTES, 'UTF-8'));
@@ -90,9 +152,15 @@ class CategoryController
         return $category->update();
     }
 
+    /**
+     * Inserts an new {@link Category}.
+     * @param string $title The title.
+     * @param string $description The description
+     * @param int|null $parent The parent root element or null
+     * @return Category|null
+     */
     public static function insert(string $title, string $description, ?int $parent): ?Category
     {
-        // TODO validation
         $category = new Category(
             0,
             htmlspecialchars($title, ENT_QUOTES, 'UTF-8'),
@@ -100,9 +168,7 @@ class CategoryController
             $parent==-1?null:$parent
         );
 
-        $category = $category->insert();
-
-        return $category;
+        return $category->insert();
     }
 
 }
--- controller/controller_order.php
+++ controller/controller_order.php
@@ -1,32 +1,51 @@
 <?php
-//TODO Comments
 
+//Add the order model.
 require_once MODEL_DIR . 'model_order.php';
 
 class OrderController
 {
 
+    /**
+     * Gets an {@link Order} by its id.
+     * @param int $id The id.
+     * @return Order|null A {@link Order} object or null, if not found.
+     */
     public static function getById(int $id): ?Order
     {
         try {
             return Order::getById($id);
         } catch (Exception $e) {
-            //TODO datetime error
+            header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
+            die();
         }
-        return null;
     }
 
+    /**
+     * Creates a new {@link Order}.
+     * @param DateTime $orderDate The date of the order.
+     * @param DateTime $deliveryDate The delivery date for the order.
+     * @param bool $paid Is the order paid?
+     * @param int $orderStateId The state id for this order.
+     * @param int $userId The id of the user who created the order.
+     * @param int $shippingAddressId The id of the shipping address.
+     * @return Order|null A new {@link Order} object or null, if an error occurred.
+     */
     public static function insert(DateTime $orderDate, DateTime $deliveryDate, bool $paid, int $orderStateId, int $userId, int $shippingAddressId): ?Order
     {
         $order = new Order(0, $orderDate, $deliveryDate, $paid, $orderStateId, $userId, $shippingAddressId);
         try {
             return $order->insert();
         } catch (Exception $e) {
-            //TODO error handling (datetime)
+            header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
+            die();
         }
-        return null;
     }
 
+    /**
+     * Calculates the delivery date. We add 10 days to the order date.
+     * @return DateTime|null A new <a href="psi_element://DateTime">DateTime</a> object or null, if an error occured.
+     */
     public static function calculateDeliveryDate(): ?DateTime
     {
         $dtZone = new DateTimeZone(DATE_TIME_ZONE);
@@ -36,49 +55,74 @@ class OrderController
             $dt->setTime(0, 0, 0, 0);
             //Now plus 10 days
             $dt->add(new DateInterval("P10D")); //TODO constant / random?
+            return $dt;
         } catch (Exception $e) {
-            //TODO handle
-        }
-        return $dt;
-    }
-
-    public static function getAmountForUser(int $userId): int
-    {
-        return Order::getAmountForUser($userId);
-    }
-
-    public static function getAllForUserInRange(int $userId, int $offset): ?array
-    {
-        try {
-            return Order::getAllForUserInRange($userId, $offset, LIMIT_OF_SHOWED_ITEMS);
-        } catch (Exception $e) {
-            //TODO handle DateTime error
-        }
-        return null;
-    }
-
-    public static function getAmountOfUsers(): int
-    {
-        return Order::getAmount();
-    }
-
-    public static function getAllInRange(int $offset, int $LIMIT_OF_SHOWED_ITEMS): ?array
-    {
-        try {
-            return Order::getAllInRange($offset, $LIMIT_OF_SHOWED_ITEMS);
-        } catch (Exception $e) {
-            //TODO 500 error
             header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
             die();
         }
     }
 
+    /**
+     * The amount of orders for a specific user.
+     * @param int $userId The id of the user.
+     * @return int The amount of orders.
+     */
+    public static function getAmountForUser(int $userId): int
+    {
+        return Order::getAmountForUser($userId);
+    }
+
+    /**
+     * Gets all order for a user in a specific range.
+     * @param int $userId The id of the user.
+     * @param int $offset The offset from where to select from the database
+     * @return array|null
+     */
+    public static function getAllForUserInRange(int $userId, int $offset): ?array
+    {
+        try {
+            return Order::getAllForUserInRange($userId, $offset, LIMIT_OF_SHOWED_ITEMS);
+        } catch (Exception $e) {
+            header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
+            die();
+        }
+    }
+
+    /**
+     * Gets the amount of all orders.
+     * @return int The total amount of orders.
+     */
+    public static function getAmountOfUsers(): int
+    {
+        return Order::getAmount();
+    }
+
+    /**
+     * Get all orders in a specific range.
+     * @param int $offset The offset from where to select from the database
+     * @param int $LIMIT_OF_SHOWED_ITEMS The amount of items, which should be selected.
+     * @return array|null
+     */
+    public static function getAllInRange(int $offset, int $LIMIT_OF_SHOWED_ITEMS): ?array
+    {
+        try {
+            return Order::getAllInRange($offset, $LIMIT_OF_SHOWED_ITEMS);
+        } catch (Exception $e) {
+            header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
+            die();
+        }
+    }
+
+    /**
+     * Deletes an order.
+     * @param Order $order The order, which should be deleted.
+     * @return bool true, if it was successfully.
+     */
     public static function delete(Order $order): bool
     {
         try {
             return $order->delete();
         } catch (Exception $e) {
-            //TODO 500 error
             header("LOCATION: " . PAGES_DIR . 'page_error.php?errorCode=500');
             die();
         }
