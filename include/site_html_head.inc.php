b82fe7d70ba406ad4c86f8f099b0ce48a55b1d98
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-07-02 16:31:50 +0200

    updated fontawesome lib

--- include/site_html_head.inc.php
+++ include/site_html_head.inc.php
@@ -24,6 +24,10 @@
 
 <!-- Load FontAwesome Icons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+      integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
 <!-- Load Custom CSS Files -->
 <link rel="stylesheet" href="<?= STYLE_DIR . "star_rating.css" ?>"/>
