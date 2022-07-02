7adeb6cb339e283de21cc23437805a8c3d9195e0
Daniel Czeschner <daniel.czeschner@web.de>
2022-07-02 20:30:55 +0200

    TODOs

--- pages/user/page_select_delivery_address.php
+++ pages/user/page_select_delivery_address.php
@@ -1,8 +1,7 @@
-<!-- TODO COMMENT -->
 <?php
 require_once "../../include/site_php_head.inc.php";
 
-// Redirect to login page, if user is not logged-in.
+// Redirect to login page, if user is not logged-in or got blocked.
 UserController::redirectIfNotLoggedIn();
 
 // Redirect, if no products are inside the cart.
@@ -11,11 +10,13 @@ if (CartProductController::getCountByUser($_SESSION["uid"]) <= 0) {
     die();
 }
 
+//Load required data
 $user = UserController::getById($_SESSION["uid"]);
 $primaryAddress = AddressController::getById($user->getDefaultAddressId());
 $deliveryAddresses = AddressController::getAllByUser($user->getId());
 $cartItems = CartProductController::getAllByUser($user->getId());
 
+//The total amount for the order
 $totalProductPrice = 0;
 ?>
 
@@ -28,6 +29,7 @@ $totalProductPrice = 0;
     <!-- file specific includes-->
     <link rel="stylesheet" href="<?= STYLE_DIR . "style_checkout.css"; ?>">
     <script src="<?= SCRIPT_DIR . "checkout_handler.js" ?>"></script>
+
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -37,6 +39,7 @@ $totalProductPrice = 0;
 <!-- main body -->
 <main class="flex-shrink-0">
 
+    <!--The purchase form-->
     <form method="post" class="needs-validation" action="<?= INCLUDE_HELPER_DIR . "helper_checkout.inc.php"; ?>"
           name="checkoutForm" id="checkoutForm" novalidate>
 
@@ -64,6 +67,7 @@ $totalProductPrice = 0;
                     </div>
 
                     <?php if (!isset($primaryAddress)): ?>
+                        <!--Default address not found-->
                         <div id="noDeliveryText">
                             <h5 class='mb-5 text-danger'>
                                 <em>There is no default address in your profile! Please select a delivery address.</em>
@@ -72,14 +76,18 @@ $totalProductPrice = 0;
 
                     <?php endif; ?>
 
+                    <!--Delivery address select container-->
                     <div class="collapse w-100" id="collapseChooseDeliveryOption">
                         <div class="form-group position-relative">
 
-                            <?php if (isset($deliveryAddresses) && count($deliveryAddresses) > 0): ?>
+                            <!--Are there some delivery addresses for the user?-->
+                            <?php if (count($deliveryAddresses) > 0): ?>
+                                <!--Add each delivery address as a selection input-->
                                 <?php foreach ($deliveryAddresses as $deliveryOption): ?>
 
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" name="delivery"
+                                               id="<?= $deliveryOption->getId() ?>"
                                                data-user="<?= UserController::getFormattedName($user) ?>"
                                                data-street="<?= $deliveryOption->getStreet() . " " . $deliveryOption->getNumber() ?>"
                                                data-city="<?= $deliveryOption->getCity() . ", " . $deliveryOption->getZip() ?>"
@@ -91,22 +99,26 @@ $totalProductPrice = 0;
                                                 echo "checked";
                                             ?>
                                         >
-                                        <label class="form-check-label">
+                                        <label class="form-check-label" for="<?= $deliveryOption->getId() ?>">
                                             <?= "<b>" . UserController::getFormattedName($user) . "</b> " . $deliveryOption->getStreet() . " " . $deliveryOption->getNumber() .
                                             ", " . $deliveryOption->getCity() . ", " . $deliveryOption->getZip() ?>
                                         </label>
                                     </div>
+
                                 <?php endforeach; ?>
-                            <?php elseif (!isset($primaryAddress)): ?>
+                            <?php else: ?>
+                                <!--No addresses found-->
                                 <h5 class='text-muted mb-5'><em>There are no addresses in your profile.</em></h5>
                                 <input type="hidden" name="delivery" value="" required>
                             <?php endif; ?>
+                            <!--Missing address validation text-->
                             <div class="invalid-tooltip opacity-75">Please choose a delivery address.</div>
                         </div>
                     </div>
 
                     <br>
 
+                    <!--Button to show the delivery address select container-->
                     <button class="btn btn-sm btn-primary w-100" type="button" data-bs-toggle="collapse"
                             data-bs-target="#collapseChooseDeliveryOption" aria-expanded="false"
                             aria-controls="collapseExample">
@@ -123,12 +135,14 @@ $totalProductPrice = 0;
 
                 </div>
                 <div class="col-lg-9 p-3 right-side align-content-center h-100">
+                    <!--Default payment method-->
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="payment" checked value="default">
-                        <label class="form-check-label">
+                        <input class="form-check-input" type="radio" name="payment" id="default" checked value="default">
+                        <label class="form-check-label" for="default">
                             Default
                         </label>
                     </div>
+
                 </div>
             </div>
 
@@ -141,6 +155,7 @@ $totalProductPrice = 0;
                 <div class="col-lg-9 p-3 right-side align-content-center h-100">
                     <div class="d-flex justify-content-center row">
                         <div class="col-md-10">
+                            <!--List of all products in cart-->
                             <?php foreach ($cartItems as $cartProduct) {
                                 $subtotal = 0;
                                 require INCLUDE_ELEMENTS_DIR . "elem_checkout_product_card.inc.php";
