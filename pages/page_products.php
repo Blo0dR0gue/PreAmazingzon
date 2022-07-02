16ca4b14bccdd10768d707b9c5cab94a2323f906
Daniel Czeschner <daniel.czeschner@web.de>
2022-07-02 20:42:07 +0200

    Comments TODOs

--- pages/page_products.php
+++ pages/page_products.php
@@ -1,16 +1,19 @@
 <!-- Page to show all products and searched products -->
-<!-- TODO comment -->
 
 <?php require_once "../include/site_php_head.inc.php"; ?>
 
 <?php
+
+//Pagination init
 $page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;                // Current pagination page number
 $offset = ($page - 1) * LIMIT_OF_SHOWED_ITEMS;                                                  // Calculate offset for pagination
 $productCount = ProductController::getAmountOfActiveProducts($_GET["search"] ?? null);      // Get the total Amount of Products
 $totalPages = ceil($productCount / LIMIT_OF_SHOWED_ITEMS);                                 // Calculate the total amount of pages
 
+//All products found
 $products = [];
 
+//Do we search for specific products?
 if (isset($_GET["search"])) {
     $products = ProductController::searchProductsInRange($_GET["search"], true, $offset, LIMIT_OF_SHOWED_ITEMS);
 } else {
@@ -22,7 +25,7 @@ if (isset($_GET["search"])) {
 <html class="h-100" lang="en">
 <head>
     <?php require_once INCLUDE_DIR . "site_html_head.inc.php"; ?>
-    <title><?= PAGE_NAME ?></title>
+    <title><?= PAGE_NAME ?> - Products</title>
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -38,10 +41,12 @@ if (isset($_GET["search"])) {
             <hr>
             <?php
             if (count($products) > 0) {
+                //Add all found products to the page.
                 foreach ($products as $product) {
                     require INCLUDE_ELEMENTS_DIR . "elem_product_card.inc.php";
                 }
             } else {
+                //No products found
                 echo "<h5 class='text-center text-muted mb-5'><i>no products found</i></h5>";
             }
             ?>
