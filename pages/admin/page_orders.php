52ff8e6658b336fe89c1b2eb37ff78145f8e4f31
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-07-02 15:59:56 +0200

    worked on manage orders page

--- pages/admin/page_orders.php
+++ pages/admin/page_orders.php
@@ -7,13 +7,11 @@ UserController::redirectIfNotAdmin();   // User is not allowed to be here.
 // pagination stuff
 $page = (isset($_GET['page']) && is_numeric($_GET['page'])) ? $_GET['page'] : 1;  // Current pagination page number
 $offset = ($page - 1) * LIMIT_OF_SHOWED_ITEMS;                                    // Calculate offset for pagination
-$userCount = OrderController::getAmountOfUsers();                              // Get the total amount of users
+$userCount = OrderController::getAmountOfUsers();                                 // Get the total amount of users
 $totalPages = ceil($userCount / LIMIT_OF_SHOWED_ITEMS);                      // Calculate the total amount of pages
 
 $orders = OrderController::getAllInRange($offset, LIMIT_OF_SHOWED_ITEMS);
-
 $orderStates = OrderStateController::getAll();
-
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +22,6 @@ $orderStates = OrderStateController::getAll();
 
     <?php require_once INCLUDE_MODAL_DIR . "modal_popup.inc.php"; ?>
     <script src="<?= SCRIPT_DIR . "admin_orders_page.js" ?>"></script>
-
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -44,31 +41,29 @@ $orderStates = OrderStateController::getAll();
         <!-- table head -->
         <thead class="thead-light">
         <tr>
-            <th scope="col" style="width: 8%"></th>
+            <th scope="col" style="width: 4%"></th>
             <th scope="col" style="width: 5%">#</th>
             <th scope="col" style="width: 8%">User ID</th>
             <th scope="col" style="width: 25%;">Username</th>
             <th scope="col" style="width: 17%">Delivery Date</th>
             <th scope="col" style="width: 17%">Order Date</th>
-            <th scope="col" style="width: 5%;">Paid?</th>
-            <th scope="col" style="width: 15%;">State</th>
+            <th scope="col" style="width: 5%;">Paid</th>
+            <th scope="col" style="width: 12%;">State</th>
         </tr>
         </thead>
 
         <!-- table body -->
         <tbody>
-        <?php
-        if (isset($orders) && count($orders) > 0):
-            foreach ($orders as $order):
-                if ($order instanceof Order):
-                    ?>
-
-                    <?php
+        <?php if (isset($orders) && count($orders) > 0) {
+            foreach ($orders as $order) {
+                if ($order instanceof Order) {
                     $user = UserController::getById($order->getUserId());
                     $orderState = OrderStateController::getById($order->getOrderStateId());
                     ?>
+
+                    <!-- table row -->
                     <tr>
-                        <td class="align-middle" data-th="">
+                        <td data-th="" class="align-middle">
                             <a class="btn btn-secondary btn-sm" data-toggle="tooltip" data-placement="left"
                                title="Delete order"
                                onclick="openConfirmModal(<?= "'Do you really want to delete the order with ID: \'" . $order->getId() . "\'and all its information?'" ?>,
@@ -78,68 +73,61 @@ $orderStates = OrderStateController::getAll();
                             </a>
                         </td>
 
-                        <td data-th="#">
+                        <td data-th="#" class="align-middle">
                             <strong><?= $order->getID(); ?></strong>
                         </td>
 
-                        <td data-th="User ID">
-                            <?php
-                            if (isset($user)) {
+                        <td data-th="User ID" class="align-middle">
+                            <?php if (isset($user)) {
                                 echo $user->getId();
                             } else {
                                 echo "#";
-                            }
-                            ?>
+                            } ?>
                         </td>
 
-                        <td data-th="Username">
-                            <?php
-                            if (isset($user)) {
+                        <td data-th="Username" class="align-middle">
+                            <?php if (isset($user)) {
                                 echo $user->getFormattedName();
                             } else {
                                 echo "User Not Found.";
-                            }
-                            ?>
+                            } ?>
                         </td>
 
-                        <td data-th="Delivery Date">
+                        <td data-th="Delivery Date" class="align-middle">
                             <?= $order->getFormattedDeliveryDate(); ?>
                         </td>
 
-                        <td data-th="Order Date">
+                        <td data-th="Order Date" class="align-middle">
                             <?= $order->getFormattedOrderDate(); ?>
                         </td>
-                        <td data-th="Paid?">
+
+                        <td data-th="Paid" class="align-middle">
                             <?= $order->isPaid() ? "Yes" : "No"; ?>
                         </td>
-                        <td data-th="State">
-                            <select class="form-select" name="stateSelector"
-                                    onchange="onOrderStateChange(this, <?= $order->getId() ?>, <?= $order->getOrderStateId(); ?>)"><!--TODO onchange-->
 
-                                <?php foreach ($orderStates as $orderStateItem): ?>
+                        <td data-th="State" class="align-middle">
+                            <select class="form-select" name="stateSelector"
+                                    onchange="onOrderStateChange(this, <?= $order->getId() ?>, <?= $order->getOrderStateId(); ?>)">
+                                <?php foreach ($orderStates as $orderStateItem) { ?>
                                     <option value="<?= $orderStateItem->getId(); ?>"
-                                        <?php
-                                        if ($orderStateItem->getId() == $orderState->getId()) {
+                                        <?php if ($orderStateItem->getId() == $orderState->getId()) {
                                             echo "selected";
-                                        } ?>
-                                    >
+                                        } ?>>
                                         <?= $orderStateItem->getLabel(); ?>
                                     </option>
-                                <?php endforeach; ?>
+                                <?php } ?>
                             </select>
                         </td>
                     </tr>
-                <?php
-                endif;
-            endforeach;
-        else:
-            ?>
+                <?php }
+            }
+        } else { // no orders in array ?>
             <tr>
                 <td colspan="8" style="text-align: center">
                     <p><em class="mb-3 text-muted">No orders are available.</em></p>
                 </td>
             </tr>
-        <?php endif; ?>
+        <?php } ?>
         </tbody>
     </table>
 </main>
