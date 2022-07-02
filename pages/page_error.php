c4b7f9b006a1816133fac70148d7fa7f019d75d4
Frederik <35914049+FrederikWolter@users.noreply.github.com>
2022-07-02 12:22:41 +0200

    worked on page_error.php

--- pages/page_error.php
+++ pages/page_error.php
@@ -10,40 +10,34 @@
 <body class="d-flex flex-column h-100">
 
 <?php
-    if(isset($_GET["errorCode"])){
-        switch ($_GET["errorCode"]){
-            case "500":
-                $errorCode = "500";
-                $errorHead = "An error has occurred.";
-                $errorMsg = "Internal server error!";
-                break;
-            case "503":
-            default:
-                $errorCode = "503";
-                $errorHead = "An error has occurred.";
-                $errorMsg = "Service unavailable!";
+if (!isset($_GET["errorCode"])) {
+    $_GET["errorCode"] = "503";
+}
 
-        }
-    }else{
+switch ($_GET["errorCode"]) {
+    case "500":
+        $errorCode = "500";
+        $errorHead = "An error has occurred.";
+        $errorMsg = "Internal server error!";
+        break;
+    case "503":
+    default:
         $errorCode = "503";
         $errorHead = "An error has occurred.";
         $errorMsg = "Service unavailable!";
-    }
+}
 ?>
 
 <!-- main body -->
 <main class="flex-shrink-0" id="content">
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="text-center">
-            <h1 class="display-1 fw-bold"><?=$errorCode?></h1>
-            <p class="fs-3"><span class="text-danger">Opps!</span> <?=$errorHead?>.</p>
-            <p class="lead">
-                <?=$errorMsg?>
-            </p>
-            <a href="<?=ROOT_DIR?>" class="btn btn-primary">Try again</a>
+            <h1 class="display-1 fw-bold"><?= $errorCode ?></h1>
+            <p class="fs-3"><span class="text-danger">Opps!</span> <?= $errorHead ?></p>
+            <p class="lead"><?= $errorMsg ?></p>
+            <a href="<?= ROOT_DIR ?>" class="btn btn-warning">Try again</a>
         </div>
     </div>
-
 </main>
 
 </body>
